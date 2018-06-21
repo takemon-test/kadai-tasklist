fa4448e4e1b48c12b433380444303d0d62437e0e
ryoma.takeda <ryoma@sukiyaki.ski>
2018-06-21 17:24:23 +0000

    CRUD task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
 
   def index
     @tasks = Task.all.page(params[:page])
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,11 +15,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
+
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が追加されませんでした'
       render :new
@@ -25,13 +27,14 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -42,7 +45,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -53,7 +56,13 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :user_id)
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   #root to: 'tasks#index'
   root to: 'toppages#index'
 
-  #resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  #resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
